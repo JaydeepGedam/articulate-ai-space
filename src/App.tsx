e2f297ec7f8495e3d2ac51ae4e727c8258a8e5b9
Jaydeep Nitin Gedam <2728991@TCSTEG.onmicrosoft.com>
2025-10-16 13:31:28 +0530

    added sample comment jaydeep gedam

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-//imports for App.jsx added by Jaydeep
+//imports for App.jsx added by Jaydeep Gedam
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
